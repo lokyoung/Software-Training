09ca8117e3863f1edc1a6120f8af522ee0a5b864
lokyoung <lokyoung@outlook.com>
2015-09-18 11:21:03 +0800

    js->jsStudy && 0918training

---
+++ jsStudy/js/1802.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("h3").click(function() {
+    $("h3").css("color", "blue");
+    $(".content").css("font-size", "10px");
+    $(".content").css("line-height", "1");
+    $("body").css("margin", "0");
+    $("body").css("padding", "0");
+  });
+});
